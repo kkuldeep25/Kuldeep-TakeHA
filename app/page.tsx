56941219e21eb0f46686fa2752188bf12d08c9b4
kkuldeep25 <165171648+kkuldeep25@users.noreply.github.com>
2024-03-28 11:00:04 +0530

    30th Update

--- app/page.tsx
+++ app/page.tsx
@@ -18,12 +18,13 @@ const SimpleComponent: React.FC = () => {
 <div> Align future roadmaps with customer needs</div>
 <div> By working together, support and product teams can deliver a superior customer experience, boosting satisfaction and loyalty. </div>
 <div> ============================================</div>
+<div>Rank your 5 most favourite Support tasks.</div>
 <div> Answer - Work with engineering teams during incidents and provide updates to internal and external stakeholders. </div>
 <div> Support managers are key communicators during incidents. We collaborate with internal teams to keep everyone informed (customers, partners, leaders) with clear, timely updates. This transparency manages expectations, minimizes impact, and empowers customers to react (like initiating disaster recovery plans). .</div>
 .<div>Post-resolution, we provide a summary and initial cause analysis. Once the full investigation concludes, we share the final report with customers and explain the post-incident review outcome. This builds trust and ensures everyone learns from the experience, preventing future occurrences.</div>
 
 <div> ===============================================</div>
-
+<div>Rank your 5 most favourite Support tasks.</div>
 <div> Answer 3 -  Work with the product team to develop a new feature based on feedback from customers. </div>
 
 <div> Going the extra mile for customers! As a frontline support manager, I believe in advocating for customer needs. While full feature development might fall outside supports scope, we can still be a bridge between customers and product management. Heres how: </div>
@@ -33,6 +34,7 @@ const SimpleComponent: React.FC = () => {
 <div> Estimated Timeframes: We strive to provide customers with estimated timeframes for feature availability whenever possible. </div>
 
 <div> ====================================</div>
+<div>Rank your 5 most favourite Support tasks.</div>
 <div> Answer 4 - Find and recruit teammates for the support team. </div>
 
 <div> Having a good teammate is like having a strong foundation. Hence its very important to build a strong support team by building a team with good teammates having the right attitude and necessary aptitude. </div>
@@ -42,6 +44,7 @@ const SimpleComponent: React.FC = () => {
 <div>  Ups and downs are part of any walk of work, one should demonstrate resilience in the face of challenging situations and perseverance to find the solutions and achieve support objectives   </div>
 
 <div> ====================================</div>
+<div>Rank your 5 most favourite Support tasks.</div>
 <div> Answer 5 Work with 3rd party partners to track down a tricky situation for a joint customer. </div>
 
 <div> To tackle business-critical issues, Id collaborate with 3rd party partners. This means bringing in the right subject matter experts and working together on all aspects of the investigation. Open communication is key, with clear expectations set for stakeholders. Id manage timelines, ensuring data gathering, diagnostics, and troubleshooting happen efficiently. Progress updates and potential escalations would be provided. Post-resolution, customer satisfaction and a partner review session ensure continuous improvement. </div>
@@ -50,32 +53,32 @@ const SimpleComponent: React.FC = () => {
 
 <div>Rank your 5 least favourite Support tasks</div>
 
-<div> Work with people to figure out if Vercel is suitable for their use case</div>
+<div> Answer - Work with people to figure out if Vercel is suitable for their use case</div>
 <div> Acknowledging the significance of pitching Vercel for diverse use cases, I must prioritize support duties. Deep involvement might compromise essential support deliverables. These tasks usually fall under customer success and solutions architecture. To aid, Ill redirect inquiries to suitable teams like solutions architects, customer success, accounts executives, ensuring thorough support while upholding my core support responsibilities. </div>
 
 <div> ========================================</div>
 
 <div>Rank your 5 least favourite Support tasks</div>
-<div> Respond to queries on Twitter, Reddit, Hacker News and other 3rd party sites</div>
+<div> Answer - Respond to queries on Twitter, Reddit, Hacker News and other 3rd party sites</div>
 <div> While it would be an honor to represent Vercel in public forums and assist Vercel community users, prioritizing other critical deliverables takes precedence. This task will have a lower priority on my to-do list. However, I am committed to supporting the Vercel community during my spare time once Ive completed my primary work 
 responsibilities. </div>
 
 <div> =========================</div>
 <div>Rank your 5 least favourite Support tasks</div>
 
-<div> Respond to 50+ support requests via email every day</div>
+<div> Answer -Respond to 50+ support requests via email every day</div>
 <div> Manual responses for 50+ support emails arent scalable and consume significant time. Support requests should funnel through valid channels like web/app ticketing portals or email-to-ticketing tool webhooks. Although email (e.g., help@abc.com, support@abc.com) is convenient for customers, workflow automation tools like Zapier are vital. They automate ticket creation and provide auto-responses with case details. </div>
 
 <div> ================================</div>
 <div>Rank your 5 least favourite Support tasks</div>
 
 
-<div> Create video tutorials to help teach users a specific feature or use case</div>
+<div> Answer -Create video tutorials to help teach users a specific feature or use case</div>
 <div> While Im enthusiastic about crafting self-help guides and documentation in web format, I have reservations about my ability to create high-quality video tutorials. To ensure professional presentation and inclusion of essential visual components, I intend to enlist the Technical Documentation team for video creation. </div>
 <div> ====================================</div>
 
 <div>Rank your 5 least favourite Support tasks</div>
-<div> Dig through logs to troubleshoot a customers broken project</div>
+<div> Answer - Dig through logs to troubleshoot a customers broken project</div>
 <div> While Im eager to assist customers, Ill prioritize timely resolutions and avoid potential delays by relying on subject matter experts or support engineers for troubleshooting. Ill study the root cause to enhance my knowledge. </div>
 
 ===============================
@@ -102,9 +105,152 @@ responsibilities. </div>
 <div>Evaluating processes for automation opportunities to reduce manual tasks and expedite turn around times. </div>
 <div>Identifying common customer issues that could be addressed through improved self-service resources (e.g., knowledge base articles, FAQs, troubleshooting guides). </div>
 <div>Identifying and addressing any gaps in existing documentation to ensure clarity, completeness, and accessibility for both customers and support staff. </div>
-
-
 <div> ========================================</div>
+<div> ========================================</div>
+
+<div>Question no 5</div>
+
+<div> Describe how you solved a challenge or technical issue that you faced in a previous role (preferably in a previous support role). How did you determine that your solution was successful? </div>
+
+
+
+
+
+<div> I encountered a significant challenge that encompassed both technical and operational aspects. A highly escalated customer reported a critical issue with a data pipeline, impacting various downstream applications crucial for business insights, including ETL processes, databases, and visualization tools like PowerBI and Tableau. </div>
+<div> The data pipeline handled processed data, with its processing logic in Spark on the Databricks platform. However, a prerequisite program on another server, managed by third-party software, complicated matters. </div>
+<div> Operationally, the customer inadvertently raised duplicate cases, resulting in negative feedback from their leadership despite our clarification that the issue lay with the third-party vendor. In response, I, as the duty manager, engaged with the customer directly, despite the unavailability of the assigned engineer due to personal reasons. </div>
+<div> While being on Customer call, In parallel, I coordinated with another engineer to ensure a comprehensive understanding of the situation. Leveraging the customers operational access to their server, we swiftly identified a Linux file permission issue in a directory, directly related to the third-party software. Despite not being responsible for that third party server instance maintenance, we provided technical recommendations, including chmod and chown commands in Linux. </div>
+<div> The customer was the server/instance owner and had permission by their leadership to carry out our recommendations (by following their emergency change process). </div>
+<div> Implementing our suggestions promptly resolved the issue, earning positive feedback from the customer and reversing the negative CSAT rating. This successful resolution underscored our commitment to customer satisfaction and willingness to collaborate, ultimately restoring trust and confidence in our support services. </div>
+
+<div> During our call, Customer ascertained and confirmed that their BI applications is getting the new updated data. </div> 
+
+<div> ==============================================</div>
+<div> When would you choose to use Edge Functions, Serverless Functions, or Edge Middleware with Vercel? </div>
+
+
+<div> I would consider using the below functions based on the use cases : </div>
+
+<div> Edge functions -  If the web apps is designated with the task which require low-latency and there is a need to process data closer to the end users location, then using Edge functions will help. If low latency and geographically distributed processing are critical, prioritize edge functions. Also consider for real-time interactions or immediate responses, consider edge functions</div>
+
+<div> Some of the use cases </div>
+<div>High quality images or content which needs optimization for faster delivery. </div>
+<div>Personalization based on user data (Content personalization) </div>
+<div>Authentication or Authorization checks</div>
+<div>Headers modification or cookies based on user location. </div>
+<div> ------------------------------------------</div>
+
+<div> Serverless Functions: Serverless functions are suited for the below use cases that does not require real-time processing or direct user interactions. Even asynchronuous workload works well with serverless functions. </div>
+
+<div> Ideal for uses cases which involve execution of backend logic, like analytic intensive workload, Dynamic data processing, High volume of Data Manipulations, compute intensive workloads, Apps requiring connections with API endpoints, Form submissions </div>
+<div> ------------------------------------------</div>
+<div> Edge Middleware: When the application needed to manipulate requests or responses customization before reaching serverless functions, then edge middleware would be the better choice. </div>
+<div> Frequently accessed data caching to get faster response time. </div>
+<div> Specific pattern based URL rewrite or routing to particular application or web page. </div>
+<div> Rate throttling/Limiting or adding security headers</div>
+
+
+<div>=======================================================</div>
+
+
+<div> Question No 6</div>
+
+<div> Question - Imagine a customer writes in requesting help with a build issue on a framework or technology that youve not seen before. How would you begin troubleshooting this and what questions would you ask the customer to understand the situation better? </div>
+
+
+<div> My approach would be as below. </div>
+
+<div> Firstly, I express gratitude to the customer for reaching out to support and promptly acknowledge the reported issue. Assuring the customer with confidence, I commit to providing all possible assistance to resolve the build issue effectively. </div>
+
+<div> Concurrently, while awaiting the requested details from the customer, I initiate the process of gathering necessary information. This includes acquiring framework specifics, complete error message logs, platform details, and a screenshot of the error message. I inquire about the development environment, any recent changes made, and aim to reproduce the issue in our lab environment for further analysis. </div>
+<div> In parallel, I conduct thorough online research by searching for the framework name, build issue keywords, and the exact error message. Exploring official documentation, community forums, company resources, and utilizing tools like Glean search, I aim to identify relevant solutions or workarounds. Additionally, I leverage internal communication channels such as Slack and seek assistance from subject matter experts. </div>
+<div> Throughout the process, I maintain transparent communication with the customer, updating them on my progress and efforts. Despite the framework being new to me, I actively seek solutions and share relevant findings, including potential workaround steps, for the customers consideration. </div>
+
+<div> ==============================================</div>
+
+<div> However Customer is not happy about the progress and reverts as below. </div>
+
+<div> Im so frustrated. Ive been trying to make this work for hours and I just cant figure it
+out. It must be a platform issue so just fix it for me instead of asking me questions. </div>
+
+<div> Answer - My response would be as below. </div>
+
+<div> I completely understand your frustration and the stress that comes with spending hours trying to resolve this issue. Please know that I empathize with your situation, and Im committed to helping you find a solution as quickly as possible. </div>
+<div> Since this framework is new to me, and considering the urgency of resolving this matter promptly, Im taking internal steps to escalate this with the utmost priority to our Backline support team for further assistance. While I coordinate with the Backline support engineer, I kindly ask for your patience and cooperation. I will promptly update you with the details for a collaborative troubleshooting session meeting details once the engineer is briefed and ready to assist. </div>
+<div> Please expect to hear back from me within the next 15 minutes with the necessary information. Thank you for your understanding and cooperation in this matter. </div>
+
+<div> =====================================</div>
+
+<div> Question No 7</div>
+<div> A customer writes in to the Helpdesk asking "How do I do a redirect from the /blog
+path to https://example.com?" Please write a reply to the customer. Feel free to add any
+information about your decision making process after the reply. </div>
+
+<div> Assuming that Customer is using Apache or tomcat. I will be using both as example. </div>
+
+
+<div> Subject: Re: Redirecting /blog path to https://example.com</div>
+
+<div> Hi CustomerName, </div>
+
+<div> Thanks for reaching out to Vercel support. My name is (My Name) and I will be assisting you with this ticket. </div>
+
+<div> To set up a redirect from /blog path to https://example.com you can utilize the below methods depending on the web server configuration and setup. </div>
+
+<div> Considering security aspects in mind, we would highly recommend using a server-side redirect. The reason being that it is the most reliable and secure way to implement a redirect. </div>
+
+<div> Please note that the below examples assume a basic server config. In Apache servers one common and recommended method is to use a web server config file such as (.htaccess) to create the redirect rule. </div>
+
+<div> -------------------------</div>
+<div> RewriteEngine On</div>
+<div> RewriteRule ^blog$ https://example.com [R=302,L] </div>
+<div> --------------------------</div>
+<div> This would need Apache restart so that the configuration is picked up. </div>
+
+<div> If tomcat is the web server -</div>
+<div> This method involves editing your tomcat applications context.xml file. Within the (Context) element, you can add a valve element configured for redirection</div>
+
+<div> Take a backup of context.xml (In linux, cp context.xml /backup/tomcat_context.bck.xml) </div>
+<div>  Open context.xml in any text editor (vi) </div>
+<div> ----------------------------</div>
+<div> Context docBase="your_app_location
+  ... other configurations ...
+  Valve className="org.apache.catalina.valves.rewrite.RewriteValve"
+    RewriteRule pattern="/blog(.*)" replacement="https://example.com/" redirect="true" /
+  /Valve
+	Context</div>
+<div> --------------------------</div>
+
+<div> Restart tomcat</div>
+
+<div> Once implemented, thoroughly test your redirects to ensure they work as expected. </div>
+
+
+<div> Syntax Explaination - </div>
+
+<div> pattern="/blog(.*)": This matches any request starting with /blog, capturing anything that follows as a group ((.*)).</div>
+<div> replacement="https://example.com/": This defines the target URL for the redirection. </div>
+<div> redirect="true": This specifies a permanent (301) redirect. </div>
+
+
+<div> Additional details 
+<div> Compared to client side redirects, the reason we highly recommend using server-side redirects is for the below reasons
+1. It ensures all users are redirected, regardless of their browser or device
+2. Server-side redirects happen before any content is loaded, potentially improving website security.
+3. Server-side redirects are generally faster than client-side redirects</div>
+
+<div>301 vs. 302 Redirect: When implementing a server-side redirect, youll need to choose between a 301 (permanent) or 302 (temporary) redirect. A 301 redirect is the best choice in this case, as it informs search engines that the /blog path has permanently moved. (in context.xml - redirect="true": This specifies a permanent (301) redirect.) </div>
+<div>If you have existing links pointing to specific blog posts under the "/blog" path, you may need to update them to point directly to the new location on your main domain. </div>
+
+<div>Hope that with the above information, you should be able to accomplish the desired result. If in case of any further assistance needed, please do not hesitate to reach out to us and we will be happy to help. </div>
+
+<div>Best regards, </div>
+<div> (My full name) </div>
+<div> (Designation) </div>
+<div>(Company Logo and helpdesk contact details) </div>
+
+
+
 
 
       
